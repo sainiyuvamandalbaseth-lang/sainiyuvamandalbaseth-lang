226745ca0dadd9bca796ff3f3ca5d96cab76266c
sainiyuvamandalbaseth-lang <sainiyuvamandalbaseth@gmail.com>
2026-07-12 12:22:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+console.log("SAINI YUVA MANDAL BASETH Website Loaded Successfully");
